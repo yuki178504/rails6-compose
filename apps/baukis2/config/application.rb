1a3baeed582f5dce068c505ad430299dd5008b10
yuki178504 <aerosmit1989x@gmail.com>
2023-02-20 16:10:30 +0900

    [add]タイムゾーンの設定とジェネレーターのソースコードの自動生成をoffにする設定を追加

--- apps/baukis2/config/application.rb
+++ apps/baukis2/config/application.rb
@@ -10,6 +10,19 @@ module Baukis2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = "Tokyo"
+    config.i18n.load_path +=
+      Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}".to_s)]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec
+      g.skip_routes true
+      g.controller_specs false
+      g.view_specs false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
