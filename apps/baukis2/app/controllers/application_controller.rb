8835ed6b4785850592032e4e89419a03467ed638
yuki178504 <aerosmit1989x@gmail.com>
2023-02-23 21:37:37 +0900

    [add]スタッフのレイアウトを定義するscssファイルを作成 アプリケーションコントローラにコントローラごとにレイアウトを適応するメソッドを作成

--- apps/baukis2/app/controllers/application_controller.rb
+++ apps/baukis2/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  private def set_layout
+    if params[:controller].present? && params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    end
+  end
 end
